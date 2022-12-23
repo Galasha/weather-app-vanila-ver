c90842eb6be9b40bf35c3497e19e3d67b4002464
Galasha <118216738+Galasha@users.noreply.github.com>
2022-12-23 23:22:43 +0200

    Add search city by Name and Coords

--- src/app.js
+++ src/app.js
@@ -5,6 +5,38 @@ function displayWeather() {
   document.getElementById("current_weather").style.display = "block";
 }
 
+function searchCity(event) {
+  event.preventDefault();
+  let searchCityName = document.querySelector("#search_city_name");
+  let city = searchCityName.value;
+
+  if (city !== null) {
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayWeather);
+    axios.get(apiUrl).then(currentTemperature);
+  } else {
+    currentCityWeather();
+  }
+}
+
+let searchForm = document.querySelector(".search_form");
+searchForm.addEventListener("submit", searchCity);
+
+function currentCityWeather(position) {
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?lon=${lon}&lat=${lat}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+  axios.get(apiUrl).then(currentTemperature);
+}
+
+function getCurrentPosition() {
+  navigator.geolocation.getCurrentPosition(currentCityWeather);
+}
+
+let locationButton = document.querySelector(".dropdown-content");
+locationButton.addEventListener("click", getCurrentPosition);
+
 function formatDate(date) {
   let daysOfWeek = [
     "Sunday",
